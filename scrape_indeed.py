2eccdae2001e643f4302876836866940b6ddeab6
Shuta Gunraku <shuta.thomas.gunrak@gmail.com>
2021-06-27 17:41:52 +0900

    Exception handlings

--- scrape_indeed.py
+++ scrape_indeed.py
@@ -2,6 +2,7 @@
 This file is a demonstration for scraping job information from indeed (https://www.indeed.com/).
 """
 __author__ = "Shuta Gunraku"
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
@@ -9,7 +10,6 @@ from selenium.webdriver.common.keys import Keys
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import lxml
 
 global indeed_ja_url
 indeed_ja_url = "https://jp.indeed.com/"
@@ -31,82 +31,72 @@ def scrape_indeed(job, location):
     options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome('./bin/chromedriver', options=options)
     driver.get(indeed_ja_url)
-    sleep(1)
+    sleep(2)
 
     # Reached the website.
     # Enter 'job' for the job category, and 'location' for the location, then click enter.
-    driver.find_element_by_xpath(
-        "/html/body/div/div[2]/div[3]/div[1]/div/div/div/form/div[1]/div[1]/div/div[2]/input").send_keys(job)
-    driver.find_element_by_xpath(
-        "/html/body/div/div[2]/div[3]/div[1]/div/div/div/form/div[2]/div[1]/div/div[2]/input").send_keys(location)
-    driver.find_element_by_xpath("/html/body/div/div[2]/div[3]/div[1]/div/div/div/form/div[3]/button").send_keys(
-        Keys.ENTER)
-    print(driver.title, "is the page.")
+    try:
+        driver.find_element_by_xpath(
+            "/html/body/div/div[2]/div[3]/div[1]/div/div/div/form/div[1]/div[1]/div/div[2]/input").send_keys(job)
+        sleep(2)
+        driver.find_element_by_xpath(
+            "/html/body/div/div[2]/div[3]/div[1]/div/div/div/form/div[2]/div[1]/div/div[2]/input").send_keys(location)
+        sleep(2)
+        driver.find_element_by_xpath("/html/body/div/div[2]/div[3]/div[1]/div/div/div/form/div[3]/button").send_keys(
+            Keys.ENTER)
+        sleep(5)
 
-    # Scrape the job info and store in data_list[]
-    data_list = []
-    page = 1
-    print("the first page", page)
-    print("url is", driver.current_url)
+        # Scrape the job info and store in data_list[]
+        data_list = []
+        page = 1
+        print("the first page", page)
+        print("url is", driver.current_url)
 
-    page_number = 0
-    while True:
-        page_number += 1
-        # Use BeautifulSoup to scrape the website.
-        res_url = driver.current_url
-        raw_html = requests.get(res_url).text
-        soup = BeautifulSoup(raw_html, "lxml")
-        page_content = soup.find("table", attrs={"id": "pageContent"})
-        # print(page_content)
-        job_cards = page_content.find_all("div", attrs={"class": "jobsearch-SerpJobCard"})
+        page_number = 0
+        while True:
+            page_number += 1
+            # Use BeautifulSoup to scrape the website.
+            res_url = driver.current_url
+            raw_html = requests.get(res_url).text
+            soup = BeautifulSoup(raw_html, "lxml")
+            page_content = soup.find("table", attrs={"id": "pageContent"})
+            # print(page_content)
+            job_cards = page_content.find_all("div", attrs={"class": "jobsearch-SerpJobCard"})
 
-        for job_card in job_cards:
-            # Get the job info by scraping with BeautifulSoup
-            data = []
-            try: job_title = job_card.find("a", attrs={"class": "jobtitle"}).get("title").replace("\n", "")
-            except: job_title = None
-            try: company_name = job_card.find("span", attrs={"class": "company"}).text.replace("\n", "")
-            except: company_name = None
-            try: location = job_card.find("div", attrs={"class": "location"}).text.replace("\n", "")
-            except: location = None
-            try: income = job_card.find("span", attrs={"class": "salaryText"}).text.replace("\n", "")
-            except: income = None
-            try: job_type = job_card.find("div", attrs={"class": "jobTypeLabelsWrapper"}).text.replace("\n", "")
-            except: job_type = None
-            data.append(company_name)
-            data.append(job_title)
-            data.append(location)
-            data.append(income)
-            data.append(job_type)
-            data_list.append(data)
+            for job_card in job_cards:
+                # Get the job info by scraping with BeautifulSoup
+                data = []
+                try: job_title = job_card.find("a", attrs={"class": "jobtitle"}).get("title").replace("\n", "")
+                except: job_title = None
+                try: company_name = job_card.find("span", attrs={"class": "company"}).text.replace("\n", "")
+                except: company_name = None
+                try: location = job_card.find("div", attrs={"class": "location"}).text.replace("\n", "")
+                except: location = None
+                try: income = job_card.find("span", attrs={"class": "salaryText"}).text.replace("\n", "")
+                except: income = None
+                try: job_type = job_card.find("div", attrs={"class": "jobTypeLabelsWrapper"}).text.replace("\n", "")
+                except: job_type = None
+                data.append(company_name)
+                data.append(job_title)
+                data.append(location)
+                data.append(income)
+                data.append(job_type)
+                data_list.append(data)
 
-        try:
-            page_button = driver.find_element_by_xpath(f"//span[@class='pn' and text()='{str(page_number + 1)}']")
-            print("The page button", page_button.text)
-            page_button.click()
-            print("url is", driver.current_url)
-            driver.get(driver.current_url)
-        except:
-            print("Exiting")
-            break
+            try:
+                page_button = driver.find_element_by_xpath(f"//span[@class='pn' and text()='{str(page_number + 1)}']")
+                print("The page button", page_button.text)
+                page_button.click()
+                print("url is", driver.current_url)
+                driver.get(driver.current_url)
+                sleep(2)
+            except:
+                print("Exiting")
+                break
 
-        # page_buttons = driver.find_elements_by_class_name("pn")
-        # print(driver.current_url)
-        # condition = False
-        # for pn in page_buttons:
-        #     try:
-        #         if pn.text == str(page_number + 1):
-        #             print("the next page is", pn.text)
-        #             pn.click()
-        #             print("url is", driver.current_url)
-        #             driver.get(driver.current_url)
-        #             condition = True
-        #     except:
-        #         continue
-        #
-        # if not condition:
-        #     print("exiting")
-        #     break
+    # Need to avoid recaptcha thus terminate execution.
+    except:
+        pass
 
     # Use pandas to get the job results table.
     df = pd.DataFrame(data_list)
@@ -120,4 +110,4 @@ def scrape_indeed(job, location):
 
 
 if __name__ == "__main__":
-    scrape_indeed("エンジニア 在宅　簡単　週4 未経験　サポート", "東京都 府中市")
+    scrape_indeed("エンジニア", "東京都 府中市")
